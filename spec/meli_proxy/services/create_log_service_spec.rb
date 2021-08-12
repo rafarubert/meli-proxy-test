9398db48e88ba66ed06bea59a9cacfe77fe8bae0
Rafael Cruz Rubert <rafael@rubert.com.br>
2021-08-12 15:10:57 -0300

    Add specs for log create

--- spec/meli_proxy/services/create_log_service_spec.rb
+++ spec/meli_proxy/services/create_log_service_spec.rb
@@ -28,5 +28,83 @@ RSpec.describe CreateLogService do
         )
       end
     end
+
+    context 'when exceeded attempts by ip' do
+      let(:log_repository_instance) { instance_double(LogRepository) }
+      let(:error_object) { { message: 'Invalid ip', status_code: 400 } }
+
+      let(:log_vo) do
+        LogVo.new(
+          ip: '127.0.0.1',
+          path: '/start',
+          fullpath: '/start'
+        )
+      end
+
+      before do
+        allow(LogRepository).to receive(:new).and_return(log_repository_instance)
+        allow(log_repository_instance).to receive(:count_ip).with(log_vo.ip).and_return(6)
+        allow(log_repository_instance).to receive(:count_path).with(log_vo.path).and_return(1)
+        allow(log_repository_instance).to receive(:count_ip_path).with(log_vo.ip, log_vo.path).and_return(1)
+      end
+
+      it {
+        expect { create_log_service }.to raise_error(
+          an_instance_of(LogError).and(having_attributes(error_object))
+        )
+      }
+    end
+
+    context 'when exceeded attempts by path' do
+      let(:log_repository_instance) { instance_double(LogRepository) }
+      let(:error_object) { { message: 'Invalid path', status_code: 400 } }
+
+      let(:log_vo) do
+        LogVo.new(
+          ip: '127.0.0.1',
+          path: '/start',
+          fullpath: '/start'
+        )
+      end
+
+      before do
+        allow(LogRepository).to receive(:new).and_return(log_repository_instance)
+        allow(log_repository_instance).to receive(:count_ip).with(log_vo.ip).and_return(1)
+        allow(log_repository_instance).to receive(:count_path).with(log_vo.path).and_return(6)
+        allow(log_repository_instance).to receive(:count_ip_path).with(log_vo.ip, log_vo.path).and_return(1)
+      end
+
+      it {
+        expect { create_log_service }.to raise_error(
+          an_instance_of(LogError).and(having_attributes(error_object))
+        )
+      }
+    end
+
+    context 'when exceeded attempts by ip and path' do
+      let(:log_repository_instance) { instance_double(LogRepository) }
+      let(:error_object) { { message: 'Invalid ip and path', status_code: 400 } }
+
+      let(:log_vo) do
+        LogVo.new(
+          ip: '127.0.0.1',
+          path: '/start',
+          fullpath: '/start'
+        )
+      end
+
+      before do
+        allow(LogRepository).to receive(:new).and_return(log_repository_instance)
+        allow(log_repository_instance).to receive(:count_ip).with(log_vo.ip).and_return(3)
+        allow(log_repository_instance).to receive(:count_path).with(log_vo.path).and_return(3)
+        allow(log_repository_instance).to receive(:count_ip_path).with(log_vo.ip, log_vo.path).and_return(5)
+      end
+
+      it {
+        expect { create_log_service }.to raise_error(
+          an_instance_of(LogError).and(having_attributes(error_object))
+        )
+      }
+    end
   end
 end
